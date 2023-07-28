2529d07ef607075e4a0689be1507e5d1cbc8e3f6
Andrew Rowley <andrew@internetdrew.com>
2023-07-28 16:24:19 -0400

    set color in theme color for status bar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ const Document = () => {
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png" />
-        <meta name="theme-color" content="#fff" />
+        <meta name="theme-color" content="#60a5fa" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"

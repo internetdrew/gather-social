4b011004d3140e1d7e32c381e932b62d7b2d034a
Andrew Rowley <andrew@internetdrew.com>
2023-07-28 23:09:49 -0400

    center sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,14 +14,16 @@ export default function Home() {
         <title>Gather Social</title>
         <meta
           name="description"
-          content="Create private social networks for your events."
+          content="Create private social networks for your special events."
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-blue-400 to-blue-300">
-        <input type="file" accept="image/x-png,image/jpeg,image/gif" />
-        <SignInButton />
-        <SignIn />
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-blue-500 to-blue-400">
+        <SignInButton mode="modal">
+          <button className="rounded-full bg-[#CB4195] px-10 py-2 font-semibold text-white duration-300 hover:bg-pink-500 hover:shadow-lg">
+            Sign in
+          </button>
+        </SignInButton>
       </main>
     </>
   );

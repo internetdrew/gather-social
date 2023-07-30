8307611c874b491f86768821d2de6ab74c0e179d
Andrew Rowley <andrew@internetdrew.com>
2023-07-30 16:14:01 -0400

    test out user icon top right

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,29 +1,7 @@
-import { useUser } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
 import Image from "next/image";
 import React from "react";
-
-const UserProfile = () => {
-  const { user } = useUser();
-
-  return (
-    <>
-      <div className="flex w-3/4 flex-col items-center space-y-2 rounded-3xl bg-slate-100 p-8 shadow-2xl">
-        {user && (
-          <div className="h-16 w-16 overflow-hidden rounded-full">
-            <Image
-              src={user.profileImageUrl}
-              width={90}
-              height={90}
-              alt="user image"
-            />
-          </div>
-        )}
-        <p className="text-xl">{user?.fullName}</p>
-      </div>
-    </>
-  );
-};
 
 const Dashboard = () => {
   return (
@@ -37,9 +15,12 @@ const Dashboard = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <section className="mx-auto flex max-w-2xl justify-center pt-20">
-          <UserProfile />
-        </section>
+        <nav className="mx-auto flex max-w-5xl justify-end p-6">
+          <div className="ml-auto">
+            <UserButton />
+          </div>
+        </nav>
+        <section className="mx-auto flex max-w-2xl justify-center pt-20"></section>
       </main>
     </>
   );

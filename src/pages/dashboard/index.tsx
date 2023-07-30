54c1721f056fd5935fbaf7c5ccb237d0b4e28719
Andrew Rowley <andrew@internetdrew.com>
2023-07-30 18:58:38 -0400

    improve dashboard ui

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -11,7 +11,7 @@ const UserProfile = () => {
     <article className="flex w-[90%] flex-col items-center justify-between space-y-10 rounded-3xl bg-slate-100 p-8 shadow-2xl sm:w-3/4">
       <div className="flex flex-col items-center justify-center">
         {user && (
-          <div className="h-12 w-12 overflow-hidden rounded-full">
+          <div className="h-16 w-16 overflow-hidden rounded-full">
             <Image
               src={user?.profileImageUrl}
               width={80}
@@ -23,11 +23,18 @@ const UserProfile = () => {
         <p className="text-xl font-semibold">Hi, {user?.firstName}!</p>
         <p className="text-slate-500">You have X active events right now.</p>
       </div>
-      <Link href={"/new"} className="w-1/2 ">
-        <button className="h-full w-full rounded-xl bg-pink-500 py-2 font-semibold text-white">
-          Host an event
-        </button>
-      </Link>
+      <div className="flex flex-col items-center justify-between gap-4 sm:flex-row ">
+        <Link href={"/new"} className="">
+          <button className="h-full w-full rounded-xl bg-pink-500 px-4 py-2 font-semibold text-white transition-shadow hover:shadow-xl">
+            Host an event
+          </button>
+        </Link>
+        <Link href={"/join"} className=" ">
+          <button className="h-full w-full rounded-xl px-4 py-2 font-semibold text-pink-500 ring-1 ring-pink-500 transition-shadow hover:shadow-xl">
+            Join an event
+          </button>
+        </Link>
+      </div>
     </article>
   );
 };
@@ -41,12 +48,10 @@ const EventsFeed = () => {
 
   return (
     <article className="flex w-[90%] flex-col space-y-2 rounded-3xl bg-slate-100 p-8 shadow-2xl sm:w-3/4">
-      <div>
+      <div className="mb-2">
         <h2 className="text-2xl font-semibold">Active Events</h2>
-        <p className="text-slate-500">
-          See active events you are hosting or attending.
-        </p>
       </div>
+      <h3 className="text-lg text-slate-500">Hosting</h3>
       {events?.map((event, idx) => (
         <div key={idx}>{event}</div>
       ))}

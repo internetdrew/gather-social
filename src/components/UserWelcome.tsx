62a20e350bd28db24f3c4e96bf0a4bffa763e8ca
Andrew Rowley <andrew@internetdrew.com>
2023-08-03 22:02:17 -0400

    convert buttons to styled links

--- src/components/UserWelcome.tsx
+++ src/components/UserWelcome.tsx
@@ -7,7 +7,7 @@ const UserWelcome = () => {
 
   return (
     <article className="flex w-[90%] flex-col items-center justify-between space-y-10 rounded-3xl bg-slate-100 p-8 shadow-2xl sm:w-3/4">
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex w-full flex-col items-center justify-center">
         {user && (
           <div className="h-16 w-16 overflow-hidden rounded-full">
             <Image
@@ -21,16 +21,18 @@ const UserWelcome = () => {
         <p className="text-xl font-semibold">Hi, {user?.firstName}!</p>
         <p className="text-slate-500">You have X active events right now.</p>
       </div>
-      <div className="flex flex-col items-center justify-between gap-4 sm:flex-row ">
-        <Link href={"/new"} className="">
-          <button className="h-full w-full rounded-xl bg-pink-500 px-4 py-2 font-semibold text-white transition-shadow hover:shadow-xl">
-            Host an event
-          </button>
+      <div className="flex w-[90%] flex-col items-center justify-between gap-4 sm:flex-row ">
+        <Link
+          href={"/new"}
+          className="h-full w-full rounded-xl bg-pink-500 px-4 py-2 text-center font-semibold text-white duration-300 hover:shadow-2xl"
+        >
+          Host an event
         </Link>
-        <Link href={"/join"} className=" ">
-          <button className="h-full w-full rounded-xl px-4 py-2 font-semibold text-pink-500 ring-1 ring-pink-500 transition-shadow hover:shadow-xl">
-            Join an event
-          </button>
+        <Link
+          href={"/join"}
+          className="h-full w-full rounded-xl px-4 py-2 text-center font-semibold text-pink-500 ring-1 ring-pink-500 duration-300 hover:bg-blue-500 hover:text-white hover:shadow-xl hover:ring-blue-500"
+        >
+          Join an event
         </Link>
       </div>
     </article>

30d8cfddfd775f1d2e4d9e7c80be6a3ce26361a6
Andrew Rowley <andrew@internetdrew.com>
2023-08-05 06:50:40 -0400

    remove static id

--- src/components/EventsFeed.tsx
+++ src/components/EventsFeed.tsx
@@ -1,7 +1,9 @@
 import { api } from "~/utils/api";
 
 const EventsFeed = () => {
-  const { data: events } = api.events.getAll.useQuery();
+  const { data } = api.events.getAll.useQuery();
+  const hostEvents = data?.hostEvents ?? [];
+  const guestEvents = data?.guestEvents ?? [];
 
   return (
     <article className="flex w-[90%] flex-col space-y-2 rounded-3xl bg-slate-100 p-8 shadow-2xl sm:w-3/4">
@@ -10,12 +12,12 @@ const EventsFeed = () => {
       </div>
       <div>
         <h3 className="text-lg text-slate-500">Hosting</h3>
-        {events?.map((event) => (
-          <div key={event.id}>{event.title}</div>
+        {hostEvents?.map((hostEvent) => (
+          <div key={hostEvent.id}>{hostEvent.title}</div>
         ))}
         <h3 className="mt-6 text-lg text-slate-500">Attending</h3>
-        {events?.map((event) => (
-          <div key={event.id}>{event.title}</div>
+        {guestEvents?.map((guestEvent) => (
+          <div key={guestEvent.id}>{guestEvent.event.title}</div>
         ))}
       </div>
     </article>
--- src/server/api/routers/events.ts
+++ src/server/api/routers/events.ts
@@ -1,8 +1,36 @@
+import { clerkClient } from "@clerk/nextjs";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const eventsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.event.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    console.log(ctx);
+    // const users = await clerkClient;
+
+    const hostEvents = await ctx.prisma.event.findMany({
+      take: 10,
+      where: {
+        hostId: "",
+      },
+    });
+
+    const guestEvents = await ctx.prisma.guestEvent.findMany({
+      take: 10,
+      where: {
+        guestIdentifier: "",
+      },
+      include: {
+        event: {
+          select: {
+            title: true,
+          },
+        },
+      },
+    });
+
+    return {
+      hostEvents,
+      guestEvents,
+    };
   }),
 });

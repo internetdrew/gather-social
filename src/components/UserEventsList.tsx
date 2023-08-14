c74770d676a9b051aabfc2c0001e193047fab7ad
Andrew Rowley <andrew@internetdrew.com>
2023-08-14 13:18:28 -0400

    remove console logs

--- src/components/UserEventsList.tsx
+++ src/components/UserEventsList.tsx
@@ -6,12 +6,10 @@ import { api } from "~/utils/api";
 const EventsList = () => {
   const setEventCount = useSetRecoilState(eventCountState);
   const { data, isLoading, isError } = api.events.getAll.useQuery();
-  console.log(data);
 
   const hostEvents = data?.hostEvents ?? [];
   const guestEvents = data?.guestEvents ?? [];
-  setEventCount(hostEvents.length + guestEvents.length);
-  console.log(hostEvents.length + guestEvents.length);
+  setEventCount(hostEvents?.length + guestEvents?.length);
 
   return (
     <article className="flex w-[90%] flex-col space-y-2 rounded-3xl bg-slate-100 p-8 shadow-2xl sm:w-3/4">
@@ -30,16 +28,16 @@ const EventsList = () => {
             <h3 className="text-lg text-slate-500">Hosting</h3>
             {hostEvents?.map((hostEvent) => (
               <div
-                key={hostEvent.id}
+                key={hostEvent?.id}
                 className="relative my-2 rounded-lg p-3 ring-1 ring-pink-500"
               >
-                <p className="text-lg font-semibold">{hostEvent.title}</p>
+                <p className="text-lg font-semibold">{hostEvent?.title}</p>
                 <p className="text-sm">
-                  Hosted by {hostEvent.hostInfo.firstName}{" "}
-                  {hostEvent.hostInfo.lastName}
+                  Hosted by {hostEvent?.hostInfo?.firstName}{" "}
+                  {hostEvent?.hostInfo?.lastName}
                 </p>
                 <Link
-                  href={`/join/events/${hostEvent.id}`}
+                  href={`/join/events/${hostEvent?.id}`}
                   className="absolute bottom-3 right-4 rounded-2xl bg-pink-500 px-6 py-1 text-sm font-medium text-white duration-300 hover:scale-105 hover:shadow-xl"
                 >
                   Join
@@ -49,16 +47,16 @@ const EventsList = () => {
             <h3 className="mt-6 text-lg text-slate-500">Attending</h3>
             {[...guestEvents, ...guestEvents]?.map((guestEvent) => (
               <div
-                key={guestEvent.id}
+                key={guestEvent?.id}
                 className="relative my-4 rounded-lg p-3 ring-1 ring-pink-500"
               >
-                <p className="text-lg font-semibold">{guestEvent.title}</p>
+                <p className="text-lg font-semibold">{guestEvent?.title}</p>
                 <p className="text-sm">
-                  Hosted by {guestEvent.hostInfo.firstName}{" "}
-                  {guestEvent.hostInfo.lastName}
+                  Hosted by {guestEvent?.hostInfo?.firstName}{" "}
+                  {guestEvent?.hostInfo?.lastName}
                 </p>
                 <Link
-                  href={`/join/events/${guestEvent.id}`}
+                  href={`/join/events/${guestEvent?.id}`}
                   className="absolute bottom-3 right-4 rounded-2xl bg-pink-500 px-6 py-1 text-sm font-medium text-white duration-300 hover:scale-105 hover:shadow-xl"
                 >
                   Join

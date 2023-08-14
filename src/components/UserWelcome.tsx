6a721a6eaeb8a705229998da6d81abca899a34e2
Andrew Rowley <andrew@internetdrew.com>
2023-08-14 13:05:09 -0400

    add conditional rendering

--- src/components/UserWelcome.tsx
+++ src/components/UserWelcome.tsx
@@ -13,17 +13,21 @@ const UserWelcome = () => {
     <article className="flex w-[90%] flex-col items-center justify-between space-y-10 rounded-3xl bg-slate-100 p-8 shadow-2xl sm:w-3/4">
       <div className="flex w-full flex-col items-center justify-center">
         {user && (
-          <div className="h-16 w-16 overflow-hidden rounded-full">
-            <Image
-              src={user?.profileImageUrl}
-              width={80}
-              height={80}
-              alt="user image"
-            />
-          </div>
+          <>
+            <div className="h-16 w-16 overflow-hidden rounded-full">
+              <Image
+                src={user?.profileImageUrl}
+                width={80}
+                height={80}
+                alt="user image"
+              />
+            </div>
+            <p className="text-xl font-semibold">Hi, {user?.firstName}!</p>
+          </>
         )}
-        <p className="text-xl font-semibold">Hi, {user?.firstName}!</p>
-        <p className="text-slate-500">{`You have ${eventCount} active events right now.`}</p>
+        {eventCount ? (
+          <p className="text-slate-500">{`You have ${eventCount} active events right now.`}</p>
+        ) : null}
       </div>
       <div className="flex w-[90%] flex-col items-center justify-between gap-4 sm:flex-row ">
         <Link

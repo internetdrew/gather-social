02be317cb243b4f465fd0a1797c4cca3a437fffc
Andrew Rowley <andrew@internetdrew.com>
2023-08-15 11:39:22 -0400

    add checkout router to app router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { postsRouter } from "./routers/posts";
 import { eventsRouter } from "./routers/events";
+import { checkoutRouter } from "./routers/checkout";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { eventsRouter } from "./routers/events";
 export const appRouter = createTRPCRouter({
   posts: postsRouter,
   events: eventsRouter,
+  checkout: checkoutRouter,
 });
 
 // export type definition of API

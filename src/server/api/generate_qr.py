725f01b21a54b87663181eb99df7d7d4950297fe
Andrew Rowley <andrew@internetdrew.com>
2023-08-25 13:58:34 -0400

    move python file to api

---
+++ src/server/api/generate_qr.py
@@ -0,0 +1,30 @@
+import sys
+import qrcode
+from base64 import b64encode
+from io import BytesIO
+from urllib.parse import quote
+
+event_id = sys.argv[1]
+
+# Construct the URL using the event ID
+event_url = f'https://gathersocial.com/event/{event_id}'
+encoded_event_url = quote(event_url, safe=':/')
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(encoded_event_url)
+qr.make(fit=True)
+
+qr_image = qr.make_image(fill_color="black", back_color="white")
+
+memory = BytesIO()
+qr_image.save(memory)
+memory.seek(0)
+
+# Convert the QR code image to base64 and print it
+qr_image_base64 = b64encode(memory.getvalue()).decode('utf-8')
+print(qr_image_base64)

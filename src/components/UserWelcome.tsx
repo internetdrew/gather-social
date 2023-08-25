073c51548da79bf3c8c0c261575594c57e843128
Andrew Rowley <andrew@internetdrew.com>
2023-08-25 11:16:44 -0400

    updated button styles and contrast

--- src/components/UserWelcome.tsx
+++ src/components/UserWelcome.tsx
@@ -10,7 +10,7 @@ const UserWelcome = () => {
   const eventCount = data?.activeEvents;
 
   return (
-    <article className="flex w-[90%] flex-col items-center justify-between space-y-10 rounded-3xl bg-slate-100 p-8 shadow-2xl sm:w-3/4">
+    <article className="flex w-[90%] flex-col items-center justify-between space-y-10 rounded-3xl bg-slate-100 p-8 shadow-2xl ring-1 ring-black sm:w-3/4">
       <div className="flex w-full flex-col items-center justify-center">
         {user && (
           <>
@@ -22,10 +22,12 @@ const UserWelcome = () => {
                 alt="user image"
               />
             </div>
-            <p className="mt-2 text-xl font-semibold">Hi, {user?.firstName}!</p>
+            <p className="mt-2 text-xl font-semibold text-slate-900">
+              Hi, {user?.firstName}!
+            </p>
           </>
         )}
-        <p className="text-center text-slate-500">
+        <p className="text-center text-slate-600">
           {eventCount
             ? `You have ${eventCount} active events right now.`
             : "You don't have any active events right now."}
@@ -34,13 +36,13 @@ const UserWelcome = () => {
       <div className="flex w-[90%] flex-col items-center justify-between gap-4 sm:flex-row ">
         <Link
           href={"/create"}
-          className="h-full w-full rounded-xl bg-pink-500 px-4 py-2 text-center font-semibold text-white duration-300 hover:scale-105 hover:shadow-2xl"
+          className="h-full w-full rounded-full bg-pink-500 px-4 py-2 text-center font-semibold ring-1 ring-black duration-300 hover:scale-105 hover:shadow-2xl"
         >
           Create an event
         </Link>
         <Link
           href={"/join"}
-          className="h-full w-full rounded-xl px-4 py-2 text-center font-semibold text-pink-500 ring-1 ring-pink-500 duration-300 hover:scale-105 hover:shadow-2xl "
+          className="h-full w-full rounded-full px-4 py-2 text-center font-semibold ring-1 ring-black duration-300 hover:scale-105 hover:shadow-2xl"
         >
           Join an event
         </Link>

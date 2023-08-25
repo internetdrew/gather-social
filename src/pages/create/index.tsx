d6624db7ba0b780e127e50f8fed91dff679f4fa0
Andrew Rowley <andrew@internetdrew.com>
2023-08-25 11:31:04 -0400

    update styles

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -6,43 +6,46 @@ const Create = () => {
   const router = useRouter();
   const [eventTitle, setEventTitle] = useState("");
 
-  const { mutate: createEvent } = api.events.create.useMutation();
+  const { mutate: createEvent, isLoading: isCreating } =
+    api.events.create.useMutation({
+      onSuccess: async () => {
+        await router.push("/home");
+      },
+    });
 
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault();
-    createEvent({ title: eventTitle });
-    void router.push("/home");
-  };
+  const maxInputChars = 60;
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <article className="flex w-[90%] flex-col space-y-2 rounded-3xl bg-slate-100 p-8 shadow-2xl sm:w-3/4 md:w-1/2 lg:w-1/3">
-        <form action="">
-          <div className="flex flex-col space-y-1">
-            <label htmlFor="event-name">Event Name</label>
-            <input
-              type="text"
-              name="event-name"
-              id="event-name"
-              value={eventTitle}
-              onChange={(e) => setEventTitle(e.target.value)}
-              placeholder="Ex. Bob and Lisa's Wedding"
-              className="rounded-xl p-3 outline-pink-500"
-            />
-          </div>
-          {eventTitle !== "" && (
-            <button
-              type="submit"
-              className="mt-4 rounded-xl bg-pink-500 px-4 py-2 text-white"
-              onClick={(e) => {
-                handleClick(e);
-              }}
-            >
-              Create a private social network for{" "}
-              <span className="font-semibold">{eventTitle}</span>
-            </button>
-          )}
-        </form>
+      <article className="flex w-[90%] flex-col space-y-2 rounded-3xl bg-slate-100 p-8 shadow-2xl ring-1 ring-black sm:w-3/4 md:w-1/2 lg:w-1/3">
+        <div className="mb-3 flex flex-col space-y-2">
+          <label htmlFor="event-name" className="font-semibold">
+            Event Name
+          </label>
+          <input
+            type="text"
+            name="event-name"
+            id="event-name"
+            value={eventTitle}
+            onChange={(e) => setEventTitle(e.target.value)}
+            placeholder="Ex. Bob and Lisa's Wedding"
+            className="rounded-xl p-3 outline-pink-500"
+            maxLength={maxInputChars}
+            disabled={isCreating}
+          />
+          <small>{`Characters remaining: ${
+            maxInputChars - eventTitle.length
+          }`}</small>
+        </div>
+        {eventTitle !== "" && (
+          <button
+            className="w-full rounded-xl bg-pink-500 px-4 py-2 ring-1 ring-black duration-300 hover:shadow-2xl"
+            onClick={() => createEvent({ title: eventTitle.trim() })}
+          >
+            Create a private social network for{" "}
+            <span className="font-semibold">{eventTitle}</span>
+          </button>
+        )}
       </article>
     </div>
   );

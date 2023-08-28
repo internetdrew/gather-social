c68c3cb9df609cfbc2b85862a4d1cb8d74f2535c
Andrew Rowley <andrew@internetdrew.com>
2023-08-28 16:08:46 -0400

    change button styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,19 +19,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-screen flex-col items-center justify-center overflow-y-hidden bg-blue-500 px-3">
-        <p className={`${modak.className} text-8xl text-[#FFA7E4]`}>Gather</p>
+        <p className={`${modak.className} text-8xl text-pink-500`}>Gather</p>
         <h1 className="mb-5 text-center text-2xl font-semibold text-white md:text-3xl">
           Create private social networks for your special events.
         </h1>
         {user.isSignedIn ? (
           <Link href={"/home"}>
-            <button className="prevent-select rounded-2xl bg-[#FFA7E4] px-10 py-2 text-xl font-semibold text-white transition-shadow hover:shadow-2xl">
+            <button className="prevent-select rounded-full bg-pink-500 px-10 py-2 text-lg font-semibold ring-1 ring-black transition-shadow hover:shadow-2xl">
               See my events
             </button>
           </Link>
         ) : (
           <SignInButton mode="modal" redirectUrl="/home">
-            <button className="prevent-select rounded-2xl bg-[#FFA7E4] px-10 py-2 text-xl font-semibold text-white transition-shadow hover:shadow-2xl">
+            <button className="prevent-select rounded-full bg-pink-500 px-10 py-2 text-xl font-semibold text-white transition-shadow hover:shadow-2xl">
               Get started
             </button>
           </SignInButton>

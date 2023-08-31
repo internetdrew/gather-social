9fcf40e665efdd70745a58ca6a4b94683176cbf8
Andrew Rowley <andrew@internetdrew.com>
2023-08-31 09:00:15 -0400

    reduce bottom margins

--- src/components/EventHeader.tsx
+++ src/components/EventHeader.tsx
@@ -17,20 +17,20 @@ const EventHeader = () => {
 
   return (
     <div className="mx-auto flex w-[95%] flex-col rounded-3xl bg-slate-100 p-6 text-center ring-1 ring-black sm:w-1/2 xl:w-1/3">
-      <h1 className="mb-10 text-3xl font-semibold">{data?.title}</h1>
+      <h1 className="mb-2 text-3xl font-semibold">{data?.title}</h1>
 
-      <div className="mt-auto text-lg">
+      <div className="text-lg">
         <p>Hosted by</p>
         <div className="flex items-center justify-center gap-1">
           <Image
             width={80}
             height={80}
             src={data.hostDetails.avatar ?? ""}
-            className="inline h-8 w-8 rounded-full"
+            className="inline h-7 w-7 rounded-full"
             alt="user profile image"
           />
           <p className="font-medium">
-            {data?.hostDetails.firstName} {data?.hostDetails.lastName}
+            {data.hostDetails.firstName} {data.hostDetails.lastName}
           </p>
         </div>
       </div>

a695485e2558a8c37c7ceb442f397aeb53ceee7f
Andrew Rowley <andrew@internetdrew.com>
2023-09-04 10:04:36 -0400

    comment out post creation procedure

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -5,11 +5,11 @@ export const postsRouter = createTRPCRouter({
   getAllForEvent: privateProcedure.query(({ ctx }) => {
     return ctx.prisma.post.findMany();
   }),
-  create: privateProcedure
-    .input(
-      z.object({
-        caption: z.string().min(1),
-      })
-    )
-    .mutation(({ ctx, input }) => {}),
+  // create: privateProcedure
+  //   .input(
+  //     z.object({
+  //       caption: z.string().min(1),
+  //     })
+  //   )
+  //   .mutation(({ ctx, input }) => {}),
 });

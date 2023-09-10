eadbfba23ec1c687801b436c4e85ab3596531f8b
Andrew Rowley <andrew@internetdrew.com>
2023-09-10 13:28:38 -0400

    add user management component to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Modak } from "next/font/google";
 import { Bars3Icon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const modak = Modak({ weight: "400", subsets: ["latin"] });
 
@@ -15,14 +17,17 @@ const Navbar: React.FC<NavbarProps> = ({ isEventFeed }) => {
         <div
           className={`${modak.className} text-3xl text-pink-500  sm:text-4xl`}
         >
-          Gather
+          <Link href={"/home"}>Gather</Link>
         </div>
-        <div className="flex gap-4">
+        <div className="flex items-center gap-4">
           {isEventFeed ? (
             <button className="rounded-xl bg-pink-500 px-4 py-2 text-sm font-semibold duration-300 hover:shadow-2xl active:scale-95">
               New Post
             </button>
           ) : null}
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
           <button className="sm:hidden">
             <Bars3Icon className="h-8 w-8 stroke-pink-500 stroke-2" />
           </button>

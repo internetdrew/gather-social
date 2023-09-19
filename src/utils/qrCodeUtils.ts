b7cf1fa5f48452aac1ab6ad3747314ac4af71c68
Andrew Rowley <andrew@internetdrew.com>
2023-09-19 14:13:24 -0400

    clean up qr code gen utils

--- src/utils/qrCodeUtils.ts
+++ src/utils/qrCodeUtils.ts
@@ -1,40 +1,4 @@
-import { spawn } from "child_process";
-import { join } from "path";
 import QRCode from "qrcode";
-
-interface Event {
-  id: string;
-  hostId: string;
-}
-
-export const generateQRCodePromise = (event: Event) => {
-  return new Promise<string>((resolve, reject) => {
-    const pythonScriptPath = join(
-      process.cwd(),
-      "/src/server/api/generate_qr.py"
-    );
-
-    const pythonProcess = spawn("python3", [pythonScriptPath, event.id]);
-
-    let qrCodeImageData = "";
-
-    pythonProcess.stdout.on("data", (data: Buffer) => {
-      qrCodeImageData += data.toString();
-    });
-
-    pythonProcess.on("error", (error) => {
-      reject(error);
-    });
-
-    pythonProcess.on("close", (code) => {
-      if (code === 0) {
-        resolve(qrCodeImageData);
-      } else {
-        reject(new Error("QR code generation failed"));
-      }
-    });
-  });
-};
 
 export const generateQRCode = async (eventId: string): Promise<string> => {
   try {

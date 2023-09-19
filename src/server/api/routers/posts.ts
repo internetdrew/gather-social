8e8d1c8ac5cecc5b1fa8962102da0235c50d3938
Andrew Rowley <andrew@internetdrew.com>
2023-09-19 14:08:15 -0400

    add rate limiting to post creation

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,6 +2,20 @@ import { z } from "zod";
 import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
 import { clerkClient } from "@clerk/nextjs";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
 
 export const postsRouter = createTRPCRouter({
   getAllForEvent: privateProcedure
@@ -71,11 +85,15 @@ export const postsRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId!;
+      const { success: withinRateLimit } = await ratelimit.limit(authorId);
+      if (!withinRateLimit) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+
       try {
         const post = await ctx.prisma.post.create({
           data: {
             caption: input.caption,
-            authorId: input.userId,
+            authorId,
             eventId: input.eventId,
           },
         });

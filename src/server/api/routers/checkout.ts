048dbc640b1ed099c507ed9478c5a5ec0112218d
Andrew Rowley <andrew@internetdrew.com>
2023-09-20 10:51:24 -0400

    install stripe cli and data to front end

--- src/server/api/routers/checkout.ts
+++ src/server/api/routers/checkout.ts
@@ -10,41 +10,41 @@ export const checkoutRouter = createTRPCRouter({
         tokens: z.number(),
       })
     )
-    .mutation(async ({ input }) => {
+    .mutation(async ({ ctx, input }) => {
       const { tokens } = input;
+      const userId = ctx.userId!;
 
-      try {
-        if (!process.env.STRIPE_SECRET_KEY) {
-          throw new Error(
-            "Stripe secret key not available in this environment"
-          );
-        }
-        const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-          apiVersion: "2022-11-15",
-          typescript: true,
-        });
-        const session = await stripe.checkout.sessions.create({
-          line_items: [
-            {
-              price: "price_1NsGgdKhtZ63tY5pEKuQwdTM",
-              quantity: tokens,
-            },
-          ],
-          mode: "payment",
-          success_url: `${process.env.SITE_URL}/home`,
-          cancel_url: `${process.env.SITE_URL}/?canceled=true`,
-        });
+      if (!process.env.STRIPE_SECRET_KEY) {
+        throw new Error("Stripe secret key not available in this environment");
+      }
 
-        if (session.url) {
-          return session.url;
-        } else {
-          throw new TRPCError({
-            code: "BAD_REQUEST",
-            message: "Invalid session url",
-          });
-        }
-      } catch (err) {
-        console.error("Error from Stripe payment intent: ", err);
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+        apiVersion: "2022-11-15",
+        typescript: true,
+      });
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price: "price_1NsGgdKhtZ63tY5pEKuQwdTM",
+            quantity: tokens,
+          },
+        ],
+        mode: "payment",
+        success_url: `${process.env.SITE_URL}/?success=true`,
+        cancel_url: `${process.env.SITE_URL}/?canceled=true`,
+        metadata: {
+          userId,
+        },
+      });
+
+      console.log(session);
+      if (session.url) {
+        return { id: session.id, url: session.url };
+      } else {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Invalid session url",
+        });
       }
     }),
 });

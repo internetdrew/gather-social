cbc7aa550ad07795023a0b8ddf1903f28cebf078
Andrew Rowley <andrew@internetdrew.com>
2023-09-22 12:24:25 -0400

    update middleware ignored route

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs/server";
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/"],
-  ignoredRoutes: ["/api/trpc/stripe", "/%3Cno%20source%3E"],
+  ignoredRoutes: ["/api/stripe", "/%3Cno%20source%3E"],
 });
 
 export const config = {

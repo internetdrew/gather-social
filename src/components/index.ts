f52d94d451cfb872e934c93e732a27fe496e604b
Andrew Rowley <andrew@internetdrew.com>
2023-09-23 13:50:02 -0400

    update export to reflect name change

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@ export { default as UserEventsList } from "./UserEventsList";
 export { default as UserWelcome } from "./UserWelcome";
 export { default as EventCard } from "./EventCard";
 export { default as LoadingSpinner } from "./LoadingSpinner";
-export { default as OptionsMenu } from "./OptionsMenu";
+export { default as EventCardOptions } from "./EventCardOptions";
 export { default as EventModal } from "./EventModal";
 export { default as EventHeader } from "./EventHeader";
 export { default as CreatePostWizard } from "./CreatePostWizard";

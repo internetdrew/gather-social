e90c24bcd6266a9b5b18e921eb61e53cc4e523df
Andrew Rowley <andrew@internetdrew.com>
2023-10-07 13:09:48 -0400

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import { useUser } from "@clerk/nextjs";
 
 const Footer = () => {
+  const user = useUser();
+
   return (
-    <div className="mx-auto mt-auto w-full max-w-screen-lg bg-red-200">
-      I am the footer and I am unfinished. Wah!
-    </div>
+    <footer className="w-full p-3">
+      <div
+        className={`mx-auto mt-auto flex w-full max-w-screen-lg items-center ${
+          user.isSignedIn ? "justify-between" : "justify-center"
+        } text-center text-lg font-semibold text-pink-400`}
+      >
+        <p className="text-famous-white">
+          Made with love by{" "}
+          <a
+            href="https://internetdrew.com"
+            target="_blank"
+            className="text-pink-400 hover:shadow-md hover:shadow-pink-400/40"
+          >
+            Internet Drew
+          </a>
+        </p>
+
+        {user.isSignedIn && (
+          <button className="hover:shadow-md hover:shadow-pink-400/40">
+            Leave Feedback
+          </button>
+        )}
+      </div>
+    </footer>
   );
 };
 

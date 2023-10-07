3617f6c91662936978afffc19d9864cdb1c91b4c
Andrew Rowley <andrew@internetdrew.com>
2023-10-07 13:51:38 -0400

    add responsive button styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 className="mb-5 text-center text-2xl font-semibold text-famous-white md:text-3xl">
           Create private social networks for your special events.
         </h1>
-        <div className="flex items-center justify-center gap-6 text-famous-black">
+        <div className="flex flex-col items-center justify-center gap-6 text-famous-black md:flex-row">
           {user.isSignedIn ? (
             <Link href={"/home"}>
               <button className="prevent-select rounded-full bg-pink-400 px-10 py-2 text-lg font-semibold ring-1 ring-black transition-shadow duration-300 hover:shadow-md hover:shadow-pink-400/40">
@@ -24,19 +24,17 @@ export default function Home() {
             </Link>
           ) : (
             <SignInButton mode="modal" redirectUrl="/home">
-              <button className="prevent-select rounded-full bg-pink-400 px-10 py-2 text-xl font-semibold transition-shadow duration-300 hover:shadow-md hover:shadow-pink-400/40">
+              <button className="prevent-select rounded-full bg-pink-400 px-10 py-2 text-lg font-semibold transition-shadow duration-300 hover:shadow-md hover:shadow-pink-400/40">
                 Get started
               </button>
             </SignInButton>
           )}
           <Link
             href={"/"}
-            className="flex items-center justify-center gap-2 rounded-full bg-famous-black px-10 py-2 text-lg font-semibold text-pink-400 ring-1 ring-pink-400 duration-300 hover:shadow-md hover:shadow-pink-400/40"
+            className="flex items-center justify-center rounded-full bg-famous-black px-9 py-2 text-lg font-semibold text-pink-400 ring-1 ring-pink-400 duration-300 hover:shadow-md hover:shadow-pink-400/40"
           >
             How it works
-            <span>
-              <ArrowDownIcon className="h-4 w-4" />
-            </span>
+            <ArrowDownIcon className="ml-1 h-4 w-4" />
           </Link>
         </div>
       </main>

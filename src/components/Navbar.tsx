d4ccd601ce3d9bc2fa311954b72cfa951e81d6b6
Andrew Rowley <andrew@internetdrew.com>
2023-10-07 13:17:34 -0400

    update nav styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,17 +20,17 @@ const Navbar: React.FC<NavbarProps> = ({ isEventFeed, postWizardRef }) => {
   };
 
   return (
-    <nav className="fixed z-10 w-full bg-[#3b82f6]">
-      <div className="mx-auto flex max-w-screen-lg items-center justify-between bg-transparent p-4">
+    <nav className="fixed z-10 w-full bg-famous-black p-3 sm:bg-opacity-0">
+      <div className="mx-auto flex max-w-screen-lg items-center justify-between">
         <div
-          className={`${modak.className} text-3xl text-pink-500  sm:text-4xl`}
+          className={`${modak.className} text-3xl text-pink-400  sm:text-4xl`}
         >
-          <Link href={"/home"}>Gather</Link>
+          <Link href={"/"}>Gather</Link>
         </div>
         <div className="flex items-center gap-4">
           {isEventFeed ? (
             <button
-              className="rounded-xl bg-pink-500 px-4 py-2 text-sm font-semibold duration-300 hover:shadow-2xl active:scale-95"
+              className="rounded-xl bg-famous-pink px-4 py-2 text-sm font-semibold duration-300 hover:shadow-2xl active:scale-95"
               onClick={handleClick}
             >
               New Post
@@ -38,11 +38,11 @@ const Navbar: React.FC<NavbarProps> = ({ isEventFeed, postWizardRef }) => {
           ) : null}
           <SignedIn>
             <span className="duration-300 hover:scale-105">
-              <UserButton />
+              <UserButton afterSignOutUrl="/" />
             </span>
           </SignedIn>
           {/* <button className="sm:hidden">
-            <Bars3Icon className="h-8 w-8 stroke-pink-500 stroke-2" />
+            <Bars3Icon className="h-8 w-8 stroke-pink-400 stroke-2" />
           </button> */}
         </div>
       </div>

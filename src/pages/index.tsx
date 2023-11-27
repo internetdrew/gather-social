a71d8129930685b71bc6d316cc414e4e7407c6c1
Andrew Rowley <andrew@internetdrew.com>
2023-11-27 08:14:22 -0500

    fix button styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { SignInButton, useUser } from "@clerk/nextjs";
 import { Modak } from "next/font/google";
 import Link from "next/link";
-import { ArrowDownIcon } from "@heroicons/react/24/outline";
 import { HowItWorks } from "~/components";
 
 const modak = Modak({ weight: "400", subsets: ["latin"] });
@@ -18,27 +17,26 @@ export default function Home() {
         <h1 className="mb-5 text-center text-2xl font-semibold text-famous-white md:text-3xl">
           Create private social networks for your special events.
         </h1>
-        <div className="mx-auto flex max-w-md flex-col items-center justify-between text-famous-black md:flex-row">
+        <div className="mx-auto flex max-w-xs flex-col items-center justify-between gap-6 text-famous-black md:max-w-md md:flex-row">
           {user.isSignedIn ? (
             <Link href={"/home"}>
-              <button className="prevent-select rounded-full bg-pink-400 px-10 py-2 text-lg font-semibold ring-1 ring-black transition-shadow duration-300 hover:shadow-md hover:shadow-pink-400/40">
+              <button className="prevent-select w-full rounded-full bg-pink-400 px-10 py-2 text-lg font-semibold ring-1 ring-black transition-shadow duration-300 hover:shadow-md hover:shadow-pink-400/40">
                 See my events
               </button>
             </Link>
           ) : (
             <SignInButton mode="modal" redirectUrl="/home">
-              <button className="prevent-select rounded-full bg-pink-400 px-10 py-2 text-lg font-semibold transition-shadow duration-300 hover:shadow-md hover:shadow-pink-400/40">
+              <button className="prevent-select w-full rounded-full bg-pink-400 px-10 py-2 text-lg font-semibold transition-shadow duration-300 hover:shadow-md hover:shadow-pink-400/40">
                 Get started
               </button>
             </SignInButton>
           )}
           <Link
             href="#how-it-works"
-            className="flex items-center justify-center rounded-full bg-famous-black px-9 py-2 text-lg font-semibold text-pink-400 ring-1 ring-pink-400 duration-300 hover:shadow-md hover:shadow-pink-400/40"
+            className="flex w-full items-center justify-center rounded-full bg-famous-black px-10 py-2 text-lg font-semibold text-pink-400 ring-1 ring-pink-400 duration-300 hover:shadow-md hover:shadow-pink-400/40"
             scroll={false}
           >
             How it works
-            <ArrowDownIcon className="ml-1 h-4 w-4" />
           </Link>
         </div>
       </section>
